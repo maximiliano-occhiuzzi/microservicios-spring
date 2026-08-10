31008810678ed79f0599b2d07c3fcaabb9d67f3e
maximiliano-occhiuzzi <omaximiliano969@gmail.com>
2026-08-10 16:36:25 -0300

    Primer commit - microservicios

---
+++ Administracion/src/test/java/com/app/Administracion/AdministracionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.Administracion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdministracionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
